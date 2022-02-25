ee245129d54504b827024ad04ed2708de86f6f2e
Victoria Filileeva <onexsan@gmail.com>
2022-02-26 00:00:29 +0300

    V-03 Вёрстка формы (шаг 2)

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -8,14 +8,13 @@
             class="order-steps__item"
             :class="{
               'order-steps__item--active': currentStep === step.id,
-              'order-steps__item--disabled': currentStep < step.id,
+              'order-steps__item--disabled': currentStep < step.id - 1,
             }"
             v-for="step in steps"
             :key="step.id"
+            @click="currentStep = step.id"
           >
-            <router-link to="/" class="order-steps__link">{{
-              step.title
-            }}</router-link>
+            <div class="order-steps__link">{{ step.title }}</div>
           </li>
         </ul>
       </div>
@@ -23,48 +22,8 @@
     <main class="order__main order-main">
       <div class="container order-main__container">
         <div class="order-main__wrapper">
-          <form action="" class="order-main__form order-form">
-            <fieldset>
-              <div
-                class="form-group order-form__form-group order-form__form-group--grid"
-              >
-                <label for="order-city" class="order-form__label">Город</label>
-                <input
-                  type="search"
-                  name="order-city"
-                  id="order-city"
-                  class="custom-input order-form__input"
-                  placeholder="Начните вводить город..."
-                />
-              </div>
-              <div
-                class="form-group order-form__form-group order-form__form-group--grid"
-              >
-                <label for="order-place" class="order-form__label"
-                  >Пункт выдачи</label
-                >
-                <input
-                  type="search"
-                  name="order-place"
-                  id="order-place"
-                  class="custom-input order-form__input"
-                  placeholder="Начните вводить пункт..."
-                />
-              </div>
-            </fieldset>
-            <fieldset>
-              <div class="form-group order-form__form-group">
-                <legend class="order-form__legend">Выбрать на карте:</legend>
-                <div class="order-form__img-placeholder">
-                  <img
-                    src="~@/assets/img/order-map.png"
-                    alt=""
-                    class="order-form__map"
-                  />
-                </div>
-              </div>
-            </fieldset>
-          </form>
+          <StepOne v-show="currentStep === 1" />
+          <StepTwo v-show="currentStep === 2" />
           <div class="order-main__details order-details">
             <h3 class="order-details__title">Ваш заказ:</h3>
             <ul class="order-details__list details-list">
@@ -74,13 +33,15 @@
                   Ульяновск, Нариманова&nbsp;42
                 </div>
               </li>
+              <li class="details-list__item details-item">
+                <div class="details-item__title">Модель</div>
+                <div class="details-item__value">Hyndai,&nbsp;i30 N</div>
+              </li>
             </ul>
             <div class="order-details__price">
               <span>Цена:</span> от 8 000 до 12 000 ₽
             </div>
-            <button class="btn order-details__btn" disabled>
-              Выбрать модель
-            </button>
+            <button class="btn order-details__btn">Выбрать модель</button>
           </div>
         </div>
       </div>
@@ -90,9 +51,13 @@
 
 <script>
 import Header from '@/components/layout/Header.vue';
+import StepOne from '@/components/pages/order/StepOne.vue';
+import StepTwo from '@/components/pages/order/StepTwo.vue';
 export default {
   components: {
     Header,
+    StepOne,
+    StepTwo,
   },
   data() {
     return {
