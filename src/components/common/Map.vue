e39d14500349260ac6825837b4d308b63de00511
Victoria Filileeva <onexsan@gmail.com>
2022-03-19 19:54:15 +0300

    F-03 Вывод пунктов выбранного города

--- src/components/common/Map.vue
+++ src/components/common/Map.vue
@@ -1,8 +1,9 @@
 <template>
   <yandex-map
+    ref="map"
     v-if="showMap"
     :coords="coords"
-    zoom="5"
+    zoom="3"
     @map-was-initialized="workWithMap"
   >
   </yandex-map>
@@ -15,7 +16,7 @@ export default {
   props: ['allMarkers', 'currentAddress'],
   data() {
     return {
-      coords: [59.939098, 30.315868],
+      coords: [55.795366, 49.105579],
       markers: [],
       pickedAddress: '',
       showMap: false,
@@ -68,13 +69,19 @@ export default {
       handler: async function (val) {
         await loadYmap();
 
-        var myMap = this.mapObj;
+        var mapComponent = this.$refs.map;
+        var myMap = mapComponent.myMap;
+        myMap.geoObjects.removeAll();
 
         for (let item of val) {
           ymaps.geocode(item).then(function (res) {
             var firstGeoObject = res.geoObjects.get(0);
+            var coords = firstGeoObject.geometry.getCoordinates();
             firstGeoObject.properties.set('iconCaption', item);
             myMap.geoObjects.add(firstGeoObject);
+            if (val.length === 1) {
+              myMap.setCenter(coords, 14, { duration: 300 });
+            }
           });
         }
 
@@ -83,6 +90,17 @@ export default {
           let coords = e.get('target')['properties'].get('iconCaption');
           self.pickedAddress = coords;
         });
+      },
+    },
+    currentAddress: {
+      handler: async function (val) {
+        if (val === '') {
+          await loadYmap();
+
+          var mapComponent = this.$refs.map;
+          var myMap = mapComponent.myMap;
+          myMap.setCenter(this.coords, 3, { duration: 300 });
+        }
       },
     },
   },
